51f4bda9f78dfaf62fe98982ceee8e310f10d2ed
lilikim5002 <130602635+lilikim5002@users.noreply.github.com>
2024-10-08 01:02:39 +0300

    .

---
+++ diplom/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <RegistrationForm />
+    </div>
+</template>
+
+<script>
+    import RegistrationForm from './components/RegistrationForm.vue'; 
+
+    export default {
+        components: {
+            RegistrationForm 
+        }
+    };
+</script>
---
+++ diplom/src/components/RegistrationForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Autorisation</h2>
+        <form @submit.prevent="registerUser">
+            <label for="username">Username:</label>
+            <input type="text" id="username" v-model="username" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required>
+
+            <button type="submit">Register</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: '',
+                password: ''
+            };
+        },
+        methods: {
+            registerUser() {
+                
+            }
+        }
+    };
+</script>
